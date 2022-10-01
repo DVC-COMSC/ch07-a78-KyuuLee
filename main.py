b245056badae416757dc607d418969ef3d98f40c
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-10-01 14:48:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ words = input('Enter the list: ').split()
 
 are = ['a', 'r', 'e']
 idxlst = []
+for i in range(len(words)):
+	# idxlst = [ words[i].find(are[j]) for j in range(len(are))] 
+	for j in range(len(are)):
+		if j == 0: 
+			prev = 0 
+		else:
+			prev = idxlst[j-1]
+		idx = words[i].find(are[j], prev) 
+		if idx == -1:
+			break
+		idxlst.append(idx)
+	else:
+		if sorted(idxlst) == idxlst:
+			print (words[i], end=' ')
+print ()
 
-# ******************************
-# Make your Code
-# ******************************
-
-# print the words that has 'a', 'r', 'e' in sequence
 
